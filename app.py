7dd3394dff9cc3974fb11eb8d29886224570c074
TDS2K5 <126385545+TDS2K5@users.noreply.github.com>
2025-08-04 23:07:59 +0530

    updated

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def home():
 # @app.route("/about")
 # def about():
 #     return render_template("index.html")
-
 # @app.route("/contact")
 # def contact():
 #     return render_template("index.html")
