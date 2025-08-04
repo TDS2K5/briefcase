70efee56bde4df39adf71710d941ee2c74f58ac5
TDS2K5 <126385545+TDS2K5@users.noreply.github.com>
2025-08-04 22:58:35 +0530

    added portfolio

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+# @app.route("/portfolio")
+# def portfolio():
+#     return render_template("index.html")
+
+# @app.route("/about")
+# def about():
+#     return render_template("index.html")
+
+# @app.route("/contact")
+# def contact():
+#     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
